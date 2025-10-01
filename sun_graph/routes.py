f330e4b7ff566e8dd9aecdbf9249681ce6b113fe
rollcloud <rollcloud@gmail.com>
2025-10-01 20:43:28 +0100

    fix: add initial branding

--- sun_graph/routes.py
+++ sun_graph/routes.py
@@ -1,3 +1,4 @@
+from starlette.responses import FileResponse
 from starlette.routing import Mount, Route
 from starlette.staticfiles import StaticFiles
 
@@ -5,7 +6,13 @@ from . import settings, views
 
 static = StaticFiles(directory=str(settings.STATIC_DIR))
 
+
+def favicon(request):
+    return FileResponse(str(settings.STATIC_DIR / "images" / "favicon.ico"))
+
+
 routes = [
     Route("/", views.home, name="home"),
+    Route("/favicon.ico", favicon, name="favicon"),
     Mount("/static", static, name="static"),
 ]
